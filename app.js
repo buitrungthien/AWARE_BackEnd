af6c1613216b3a3f6c6e7e0f18b63db68c303310
buitrungthien <thienbt95@gmail.com>
2019-07-04 16:33:14 +0700

    first commit - Web API made by NodeJs

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/user');
+const login = require('./routes/login');
+const config = require('config');
+const error = require('./middleware/error');
+const app = express();
+require('express-async-errors');
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost/awaredb')
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch(err => console.error('Could not coneect to MongoDB...', err));
+
+// Middlewares
+app.use(express.json());
+app.use('/api/users', users);
+app.use('/api/login', login);
+
+app.use(error);
+
+//PORT
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
