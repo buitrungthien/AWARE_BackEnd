e98abb47e4eec8556c207511e90760ed8be3e34d
buitrungthien <thienbt95@gmail.com>
2019-07-09 01:59:59 +0700

    added account setting route

--- models/user.js
+++ models/user.js
@@ -36,7 +36,7 @@ userSchema.methods.generateAuthToken = function () {
 
 const User = mongosoe.model('User', userSchema);
 
-function validateUser(user) {
+function validateNewUser(user) {
     const schema = {
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
@@ -46,5 +46,15 @@ function validateUser(user) {
     return Joi.validate(user, schema);
 }
 
+function validateChangeInfoUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+    };
+
+    return Joi.validate(user, schema);
+}
+
 exports.User = User;
-exports.validate = validateUser;
+exports.validateNewUser = validateNewUser;
+exports.validateChangeInfoUser = validateChangeInfoUser;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const { User, validate } = require('../models/user');
+const { User, validateNewUser, validateChangeInfoUser } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -12,7 +12,7 @@ router.get('/me', auth, async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
+    const { error } = validateNewUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
@@ -26,6 +26,26 @@ router.post('/', async (req, res) => {
     await user.save();
 
     res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.patch('/', auth, async (req, res) => {
+    let updatedUser = {};
+    if (req.body.email) {
+        const user = await User.findOne({email: req.body.email});
+        if (user && (user._id != req.user._id)) return res.status(400).send('This email is already exist.');
+        updatedUser = await User.findOneAndUpdate({_id: req.user._id}, {email} = req.body).select('-password');
+    } else {
+        updatedUser = await User.findOneAndUpdate({_id: req.user._id}, {name} = req.body).select('-password');
+    }
+    res.send(updatedUser);
+});
+
+router.put('/', auth, async (req, res) => {
+    let updatedUser = {};
+    const user = await User.findOne({email: req.body.email});
+    if (user && (user._id != req.user._id)) return res.status(400).send('This email is already exist.');
+    updatedUser = await User.findOneAndUpdate({_id: req.user._id}, {email, name} = req.body).select('-password');
+    res.send(updatedUser);
 });
 
 module.exports = router;
