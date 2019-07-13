21bd8d39df9615d8145daf3e862be89f5056c3c2
buitrungthien <thienbt95@gmail.com>
2019-07-14 01:38:37 +0700

    minor changes

--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
         cb(null, './images');
     },
     filename: function (req, file, cb) {
-        cb(null, new Date().toISOString().replace(/:|\./g, '') + file.originalname);
+        cb(null, new Date().toISOString().replace(/:|\./g, '') + Math.random() + file.originalname);
     }
 });
 
